87cfc2c4ff7385214d16de319b55c6ee8adb8b70
tadeoalmiron <119013564+tadeoalmiron@users.noreply.github.com>
2023-08-03 23:20:42 -0300

    estado actual del proyecto

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const mongoose = require('mongoose'); // Importar Mongoose
 
 const app = express();
 const PORT = 8080;
 
-const ProductManager = require('./ProductManager');
-const productManager = new ProductManager('productos.json');
+const dbUrl = 'mongodb+srv://<tadeoalmiron95>:<pochoclo74>@<proyectobackend.rllilts.mongodb.net>';
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conexión a MongoDB Atlas establecida');
+    // Iniciar el servidor
+    http.listen(PORT, () => {
+      console.log(`Servidor escuchando en el puerto ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error al conectar a MongoDB Atlas:', error);
+  });
+
+// Importar el modelo de producto
+const ProductModel = require('./ProductModel');
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -18,66 +33,75 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 // Ruta raíz para listar todos los productos
-app.get('/api/products', (req, res) => {
-  const products = productManager.getProducts();
-  res.json(products);
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await ProductModel.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener los productos' });
+  }
 });
 
 // Ruta para obtener un producto por su id
-app.get('/api/products/:pid', (req, res) => {
-  const productId = parseInt(req.params.pid);
-  const product = productManager.getProductById(productId);
-
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ message: 'Producto no encontrado' });
+app.get('/api/products/:pid', async (req, res) => {
+  const productId = req.params.pid;
+  try {
+    const product = await ProductModel.findById(productId);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Producto no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener el producto' });
   }
 });
 
 // Ruta para actualizar un producto por su id
-app.put('/api/products/:pid', (req, res) => {
-  const productId = parseInt(req.params.pid);
+app.put('/api/products/:pid', async (req, res) => {
+  const productId = req.params.pid;
   const updatedProduct = req.body;
-  const product = productManager.updateProduct(productId, updatedProduct);
-
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ message: 'Producto no encontrado' });
+  try {
+    const product = await ProductModel.findByIdAndUpdate(productId, updatedProduct, { new: true });
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Producto no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error al actualizar el producto' });
   }
 });
 
 // Ruta para eliminar un producto por su id
-app.delete('/api/products/:pid', (req, res) => {
-  const productId = parseInt(req.params.pid);
-  const product = productManager.deleteProduct(productId);
-
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ message: 'Producto no encontrado' });
+app.delete('/api/products/:pid', async (req, res) => {
+  const productId = req.params.pid;
+  try {
+    const product = await ProductModel.findByIdAndRemove(productId);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Producto no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar el producto' });
   }
 });
 
 // Ruta raíz para agregar un nuevo producto
-app.post('/api/products', (req, res) => {
+app.post('/api/products', async (req, res) => {
   const product = req.body;
-  // Generar un ID autogenerado para el nuevo producto
-  const newProductId = Date.now().toString();
-  // Asignar el ID al producto
-  product.id = newProductId;
-  // Establecer el estado por defecto como true si no se proporciona
-  product.status = product.status !== undefined ? product.status : true;
-  // Agregar el producto al gestor de productos
-  productManager.addProduct(product);
-  res.status(201).json(product);
+  try {
+    const newProduct = await ProductModel.create(product);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al agregar el producto' });
+  }
 });
 
 // Configurar el evento de conexión del socket
 io.on('connection', (socket) => {
   console.log('Un cliente se ha conectado');
-
 });
 
 // Iniciar el servidor
